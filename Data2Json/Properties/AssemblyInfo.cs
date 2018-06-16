d4ba7cad0c3925bdb55ec519fee3682eda8ee3ce
eric <eric@DESKTOP-UPDMRGO>
2018-06-17 02:55:44 +0800

    初步实现

--- Data2Json/Properties/AssemblyInfo.cs
+++ Data2Json/Properties/AssemblyInfo.cs
@@ -13,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: Fiddler.RequiredVersion("5.0.20181.14850")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
